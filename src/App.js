eeec0cac6ffd15aa8ae7030a816d86aade38c72c
redirect-manager-user <boris.d.jong@belsimpel.nl>
2025-07-15 13:57:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -314,7 +314,7 @@ export default function App() {
                       --primary: 222.2 47.4% 11.2%;
                       --primary-foreground: 210 40% 98%;
                       --muted: 210 40% 96.1%;
-                      --muted-foreground: 215.4 16.3% 46.9%;
+                      --muted-foreground: 215.4 16.3% 70%;
                       --accent: 210 40% 96.1%;
                       --accent-foreground: 222.2 47.4% 11.2%;
                       --destructive: 0 84.2% 60.2%;
